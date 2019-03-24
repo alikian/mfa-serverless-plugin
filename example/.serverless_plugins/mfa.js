2caa5603fbf874c526083e57d81cc37c0ceab59d
ali.kian@gmail.com <RyanKian!1>
2019-03-24 13:25:02 -0700

    working

--- example/.serverless_plugins/mfa.js
+++ example/.serverless_plugins/mfa.js
@@ -1,60 +1,36 @@
 'use strict';
 
+const speakeasy = require("speakeasy");
+
 class ServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
 
-    this.commands = {
-      mfa: {
-        usage: 'Helps you start your first Serverless plugin',
-        lifecycleEvents: [
-          'sts',
-        ],
-        options: {
-          arn: {
-            usage:
-              'Specify Arn or Serial Number of your MFA'
-              + '(e.g. "--arn \'arn:aws:iam::123456789012:sms-mfa/username\'" or "-a \'arn:aws:iam::123456789012:sms-mfa/username\'")',
-            required: true,
-            shortcut: 'a',
-          },
-          token: {
-            usage:
-              'Specify token your MFA'
-              + '(e.g. "--token {your_token}" or "-t {your_token}")',
-            required: true,
-            shortcut: 't',
-          },
-        },
-      },
-    };
-
     this.hooks = {
-      'mfa:sts': this.mfa.bind(this),
+      'package:finalize': this.mfa.bind(this),
     };
   }
 
   async mfa() {
-    this.serverless.cli.log('Calling Credential');
-    const credentials = this.serverless.providers.aws.getCredentials();
+    this.serverless.cli.log('get token');
+    
+    var token = speakeasy.totp({
+      secret: process.env.AWS_MFA_KEY,
+      encoding: 'base32'
+    });
+    
+    this.serverless.cli.log('token: ' + token);
 
-    this.serverless.cli.log('Called Credential' + credentials);
+    const credentials = this.serverless.providers.aws.getCredentials();
 
     this.sts = new this.serverless.providers.aws.sdk.STS(credentials);
 
-    this.serverless.cli.log('Called new STS' + this.sts);
-
-
     var params = {
       DurationSeconds: 3600,
-      SerialNumber: this.options['arn'],
-      TokenCode: this.options['token']
+      SerialNumber: process.env.AWS_MFA_SERIAL_NUMBER,
+      TokenCode: token
     };
-
-    this.serverless.cli.log('params');
-    this.serverless.cli.log(this.options['arn']);
-    this.serverless.cli.log(this.options['token']);
 
     let stsCredentials = await this.sts.getSessionToken(params).promise();
     this.serverless.cli.log('sts success' );
@@ -63,8 +39,8 @@ class ServerlessPlugin {
     process.env.AWS_SECRET_ACCESS_KEY = stsCredentials.Credentials.SecretAccessKey;
     process.env.AWS_SESSION_TOKEN = stsCredentials.Credentials.SessionToken;
 
-    this.serverless.cli.log('stsCredentials' + stsCredentials.Credentials.AccessKeyId);
-
+    // Clear cached credentials
+    this.serverless.providers.aws.cachedCredentials = null;
 
 
   }
--- example/handler.js
+++ example/handler.js
@@ -4,7 +4,7 @@ module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.1! Your function executed successfully!',
       input: event,
     }),
   };
