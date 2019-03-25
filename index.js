7bc6c9c2b00d51a49595865c77dcc8e3b25bf651
ali.kian@gmail.com <RyanKian!1>
2019-03-24 20:49:47 -0700

    more clean uo

--- index.js
+++ index.js
@@ -15,21 +15,23 @@ class ServerlessPlugin {
   async mfa() {
     
     if (!process.env.AWS_MFA_KEY){
-      this.serverless.cli.log('AWS_MFA_KEY  environment variable does not existing skipping mfa');
+      this.serverless.cli.log('AWS_MFA_KEY  environment variable is missing, skip mfa');
       return;
     }
     if (!process.env.AWS_MFA_SERIAL_NUMBER){
-      this.serverless.cli.log('AWS_MFA_SERIAL_NUMBER environment variable does not existing skipping mfa');
+      this.serverless.cli.log('AWS_MFA_SERIAL_NUMBER environment variable is missing, skip mfa');
       return;
     }
 
     this.serverless.cli.log('get token');
     
+    // Get Token
     var token = speakeasy.totp({
       secret: process.env.AWS_MFA_KEY,
       encoding: 'base32'
     });
-    
+
+    // Get current credential for STS getSession
     const credentials = this.serverless.providers.aws.getCredentials();
 
     this.sts = new this.serverless.providers.aws.sdk.STS(credentials);
@@ -43,12 +45,13 @@ class ServerlessPlugin {
     let stsCredentials = await this.sts.getSessionToken(params).promise();
     this.serverless.cli.log('sts success' );
 
+    // Clear cached credentials
+    this.serverless.providers.aws.cachedCredentials = null;
+
+    // Set AWS CLI Environment variable, any concequence AWS call will use these crendentials
     process.env.AWS_ACCESS_KEY_ID = stsCredentials.Credentials.AccessKeyId;
     process.env.AWS_SECRET_ACCESS_KEY = stsCredentials.Credentials.SecretAccessKey;
     process.env.AWS_SESSION_TOKEN = stsCredentials.Credentials.SessionToken;
-
-    // Clear cached credentials
-    this.serverless.providers.aws.cachedCredentials = null;
 
   }
 
